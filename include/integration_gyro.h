ed2d7718b026cbb95268fb2c07d1b6c06c5c3871
ferdaousse <ferdaousse.chakir@gmail.com>
2014-10-23 16:33:57 +0200

    Integration from a CSV file

--- include/integration_gyro.h
+++ include/integration_gyro.h
@@ -6,17 +6,17 @@ class integration_gyro
 public:
 
     integration_gyro();
-    integration_gyro(const float Vx[5],const float Vy[5],const float Vz[5],const double T[5]);
-    float* Integration_gyro_X();
-    float* Integration_gyro_Y();
-    float* Integration_gyro_Z();
+    integration_gyro(float** S,int n);
+    float* Integration_gyro_X(int n);
+    float* Integration_gyro_Y(int n);
+    float* Integration_gyro_Z(int n);
     void test();
 
 private:
     float* VX;
     float* VY;
     float* VZ;
-    double* t;
+    float* t;
 
 
 };
