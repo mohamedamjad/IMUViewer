fdc37b53338ff80f948f4922dce1c4b817291068
cwamgis <cwamgis@hotmail.com>
2014-11-05 11:59:25 +0100

    Update tableaudebord.cpp

--- src/tableaudebord.cpp
+++ src/tableaudebord.cpp
@@ -3,23 +3,61 @@
 
 
 TableauDeBord::TableauDeBord()
-{
-
+{   
     std::cout<<"Constructeur tdb"<<std::endl;
     // Lecture du fichier des données centrale
     CSV donneesCentrale;
     double **donneesBrutes   = donneesCentrale.readCSV(fichierCsv);
+
     // Nombre de lignes du fichier
     _nbEch = donneesCentrale.getNbLines();
     // Cree un vecteur de signaux avec toutes les données
     creeVecteurSignaux(donneesBrutes,freqFiltre,freqEch,true);
-
+    calculeFenetreCentrale();
 }
 
 
 TableauDeBord::~TableauDeBord()
 {}
 
+// Calcule la fenête d'évolution de la centrale en simulant son évolution
+void TableauDeBord::calculeFenetreCentrale()
+{
+    double x,y,z=0.0;
+    _coinInferieur.append(INFINITY);
+    _coinInferieur.append(INFINITY);
+    _coinInferieur.append(INFINITY);
+    _coinSuperieur.append(-INFINITY);
+    _coinSuperieur.append(-INFINITY);
+    _coinSuperieur.append(-INFINITY);
+
+
+    // Parcours de toutes les données
+    for (int i=0;i<_nbEch-2;i++)
+    {
+        x+=_signaux[0]->getSignalDoubleIntegre(i);
+        y+=_signaux[1]->getSignalDoubleIntegre(i);
+        z+=_signaux[2]->getSignalDoubleIntegre(i);
+
+        if (x<_coinInferieur[0])_coinInferieur[0]=x;
+        if (y<_coinInferieur[1])_coinInferieur[1]=y;
+        if (z<_coinInferieur[2])_coinInferieur[2]=z;
+
+        if (x>_coinSuperieur[0])_coinSuperieur[0]=x;
+        if (y>_coinSuperieur[1])_coinSuperieur[1]=y;
+        if (z>_coinSuperieur[2])_coinSuperieur[2]=z;
+    }
+}
+
+QVector<double> TableauDeBord::getCoinInferieur()
+{
+    return _coinInferieur;
+}
+
+QVector<double> TableauDeBord::getCoinSuperieur()
+{
+    return _coinSuperieur;
+}
 
 void TableauDeBord::creeVecteurSignaux(double** donneesBrutes,  FrequencyType uneFreqFiltre, FrequencyType uneFreqEch,bool reEchantillone)
 {
@@ -78,8 +116,33 @@ void TableauDeBord::majCentrale()
     _IMU._gyroI[1]= _signaux[4]->getSignalIntegre(iCourant);
     _IMU._gyroI[2]= _signaux[5]->getSignalIntegre(iCourant);
 
+    // Orientation depuis le gyro
+    _IMU._orientation[0]+=_IMU._gyroI[0];
+    _IMU._orientation[1]+= _IMU._gyroI[1];
+    _IMU._orientation[2]+= _IMU._gyroI[2];
+
+    // Postion depuis l'acceleromètre
+    _IMU._position[0]+= _IMU._acc2I[0];
+    _IMU._position[1]+= _IMU._acc2I[1];
+    _IMU._position[2]+= _IMU._acc2I[2];
+
+
     // Incrémentation de l'indice de parcours des données
-    iCourant = (iCourant < (_signaux.at(0)->getTaille() -1)) ? iCourant+1:0;
-    std::cout<< iCourant<< "acc = " << _IMU._acc[0]<< std::endl;
+    iCourant = (iCourant < (_signaux.at(0)->getTaille() -2)) ? iCourant+1:0;
+
+    if (iCourant==0)
+    {
+        // Orientation depuis le gyro
+        _IMU._orientation[0]=0;
+        _IMU._orientation[1]=0;
+        _IMU._orientation[2]=0;
+
+        // Postion depuis l'acceleromètre
+        _IMU._position[0]=0;
+        _IMU._position[1]=0;
+        _IMU._position[2]=0;
+
+    }
+
 }
 
