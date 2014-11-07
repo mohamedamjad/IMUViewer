7599bd95272f5d91204ec7f5953649b5344eff16
Mohamed Amjad LASRI <mohamed-amjad.lasri@ensg.eu>
2014-11-07 15:13:22 +0100

    Update mainwindow.cpp

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -14,21 +14,25 @@ MainWindow::MainWindow(QWidget *parent) :
     this->findChild<AccCapteurGL*>("glCapteurAcc")->setCentrale(&_pTdb->_IMU);
     this->findChild<GyroCapteurGL*>("glCapteurGyro")->setCentrale(&_pTdb->_IMU);
     this->findChild<MagneCapteurGL*>("glCapteurMagne")->setCentrale(&_pTdb->_IMU);
-    this->findChild<gyrograph*>("glSignalAcc")->setCentrale (&_pTdb->_IMU);
-    
-        this->findChild<gyrograph*>("glSignalAcc")->setCentrale (&_pTdb->_IMU);
-    this->findChild<gyrograph*>("glSignalAcc")->setsignalIndex (0);
-
-    this->findChild<gyrograph*>("glSignalGyro")->setCentrale (&_pTdb->_IMU);
-    this->findChild<gyrograph*>("glSignalGyro")->setsignalIndex (3);
-
-    this->findChild<gyrograph*>("glSignalMagne")->setCentrale (&_pTdb->_IMU);
-    this->findChild<gyrograph*>("glSignalMagne")->setsignalIndex (6);
 
     _pcGL = this->findChild<PrincipalCapteurGL*>("glPrincipal");
     _pcGL->setCentrale(&_pTdb->_IMU);
     // + fenetre d'évolution de la centrale
     _pcGL->setFenetreEvolutionCentrale(_pTdb->getCoinInferieur(),_pTdb->getCoinSuperieur());
+
+    this->findChild<gyrograph*>("glSignalAcc")->setCentrale (&_pTdb->_IMU);
+    this->findChild<gyrograph*>("glSignalAcc")->setTableauDeBord (_pTdb);
+    this->findChild<gyrograph*>("glSignalAcc")->setsignalIndex (0);
+
+    this->findChild<gyrograph*>("glSignalGyro")->setCentrale (&_pTdb->_IMU);
+    this->findChild<gyrograph*>("glSignalGyro")->setTableauDeBord (_pTdb);
+    this->findChild<gyrograph*>("glSignalGyro")->setsignalIndex (3);
+
+    this->findChild<gyrograph*>("glSignalMagne")->setCentrale (&_pTdb->_IMU);
+    this->findChild<gyrograph*>("glSignalMagne")->setTableauDeBord (_pTdb);
+    this->findChild<gyrograph*>("glSignalMagne")->setsignalIndex (6);
+
+
 
     // Mise à jour de la centrale inertielle en suivant le timer
     QObject::connect(timer, SIGNAL(timeout()), _pTdb, SLOT(majCentrale()));
@@ -41,7 +45,7 @@ MainWindow::MainWindow(QWidget *parent) :
     QObject::connect(timer, SIGNAL(timeout()), this->findChild<gyrograph*>("glSignalGyro"), SLOT(updateGL()));
     QObject::connect(timer, SIGNAL(timeout()), this->findChild<gyrograph*>("glSignalMagne"), SLOT(updateGL()));
     // timer calé sur la fréquence d'échantillonage des signaux
-    timer->start(1000/freqEch);
+    timer->start(100);
 }
 
 
